5426e1dff4fe786471aaa3dc9c23a0cffbd22416
MR-KIMDAUN <people5474@naver.com>
2022-03-30 17:02:48 +0900

    20220330

---
+++ src/main/java/test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        // 내가 입력한 숫자가 홀인지 짝인지 출력해주는 프로그햄
+        // 10 -> 짝입니다. 11 -> 홀입니다.
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("숫자를 입력하세요. \n");
+
+        int n = sc.nextInt();
+        // boolean m = (n % 2 == 0);
+
+
+        if (n % 2 == 0) {
+            System.out.printf("짝입니다.");
+        }else {
+            System.out.printf("홀입니다.");
+        }
+    }
+}
